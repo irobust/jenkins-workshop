88d9e758828ee072a609ea6e783209b6e25661a6
phanupong <phanupong@irobust.co.th>
2021-05-28 11:37:18 +0700

    update pipeline for email notification

--- Connect-to-Git/pipeline.groovy
+++ Connect-to-Git/pipeline.groovy
@@ -13,16 +13,6 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                sh 'false'
-            }
-
-            post {
-                success {
-                    sh 'echo build succees'
-                }
-                failure {
-                    sh 'echo build fail'
-                }
             }
         }
     }
--- Email-Notification/pipeline.groovy
+++ Email-Notification/pipeline.groovy
@@ -6,9 +6,16 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
+                sh 'false'
             }
 
             post {
+                success {
+                    sh 'echo build succees'
+                }
+                failure {
+                    sh 'echo build fail'
+                }
                 always {
                     emailext attachLog: true, 
                              compressLog: true,
